ac1423a6b61220ac7154a6b7dea81b96595a5273
oyvikjo <oyvindjohnsen@outlook.com>
2017-01-25 21:31:51 -0800

    Commited

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,35 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('#testjs').text("It worked!");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
+}
+
+function projectClick(e) {
+	
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00")
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if(!description.is(":visible")){
+    	description.fadeIn();
+    } else {
+    	description.fadeOut();
+    }
+
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+       description.hide();
+       description.fadeIn();
+    }
 }
